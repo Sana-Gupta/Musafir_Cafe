ba1b4a44e62b687035a9eaa1a786ee22d82c7287
sheetal <56538155+Sheetal1614@users.noreply.github.com>
2020-10-29 11:40:04 +0530

    Add files via upload

---
+++ chunks/description.php
@@ -0,0 +1,13 @@
+<section class="fdes">
+		<div class="section white center">
+			<h2 class="header" style="padding:20px; padding-bottom: 30px;">Cafe Powered By 4 IT Girls</h2>
+		      <div class="row container center">
+		        <div class="col center l8 s12">
+				<p> Love//Code and Gossip !! </p>
+		        </div>
+		        <div class="col center l4 s12">
+		        	<img height="300" width="auto" style="object-fit: contain;" src="images/17964.jpg" alt="">
+		        </div>
+		        
+		      </div>
+	</section>
